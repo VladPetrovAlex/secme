717626e696a1a1771e66efcf181661efc19e7ce4
VladPetrovAlex <vlad.secme@gmail.com>
2021-11-15 23:29:01 +0000

    Autocommit per build...

--- inc/libcwrap.h
+++ inc/libcwrap.h
@@ -13,3 +13,6 @@ extern ssize_t secme_sendto(int fd, const char *buf, size_t size, const struct s
 
 __attribute__((warn_unused_result))
 extern ssize_t secme_recvfrom(int fd, char *buf, size_t size, struct sockaddr *addr, socklen_t *addrlen);
+
+__attribute__((warn_unused_result))
+int secme_epoll_wait(int epfd, struct epoll_event *events, size_t eventmax, int timeout);
