b3eec6577e6a2e24764da143fe109bfced15add0
VladPetrovAlex <vlad.secme@gmail.com>
2021-11-17 00:25:37 +0000

    secme_prod_cons 0.1

--- inc/prod_cons.h
+++ inc/prod_cons.h
@@ -9,24 +9,31 @@ struct {                      \
   atomic_size_t count;        \
 }
 
-#define secme_prod_cons_init(pc) \
-{                                \
-  (pc)->cons = (__typeof((pc)->cons))(pc);           \
-  (pc)->prod = (__typeof((pc)->prod))(pc);           \
-  (pc)->count = 0;               \
+#define secme_prod_cons_init(pc)           \
+{                                          \
+  (pc)->cons = (__typeof((pc)->cons))(pc); \
+  (pc)->prod = (__typeof((pc)->prod))(pc); \
+  (pc)->count = 0;                         \
 }
 
-#define secme_prod_cons_prod(pc, p)  \
-{                                    \
-  (pc)->prod->next = p;              \
-  (pc)->prod = p;                    \
-  atomic_fetch_add(&(pc)->count, 1); \
+#define secme_prod_cons_prod(pc, p)    \
+{                                      \
+  (pc)->prod = (pc)->prod->next = (p); \
+  atomic_fetch_add(&(pc)->count, 1);   \
 }
 
-#define secme_prod_cons_cons(pc)                 \
-({                                               \
-  __typeof((pc)->cons) res__ = (pc)->cons->next; \
-  (pc)->cons = (pc)->cons->next;                 \
-  atomic_fetch_sub(&(pc)->count, 1);             \
-  res__;                                         \
+#define secme_prod_cons_count(pc) \
+  (atomic_load(&(pc)->count))
+
+#define secme_prod_cons_empty(pc) \
+  (0 == secme_prod_cons_count((pc)))
+
+#define secme_prod_cons_cons(pc)       \
+({                                     \
+  __typeof((pc)->cons) res__ = NULL;   \
+  if(!secme_prod_cons_empty((pc))) {   \
+    res__ = (pc)->cons->next;          \
+    atomic_fetch_sub(&(pc)->count, 1); \
+  }                                    \
+  res__;                               \
 })
