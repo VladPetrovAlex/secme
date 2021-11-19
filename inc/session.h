93706fd827c40d2ec767a2205c333cdfdf0b5847
VladPetrovAlex <vlad.secme@gmail.com>
2021-11-19 11:21:37 +0000

    nl

--- inc/session.h
+++ inc/session.h
@@ -2,9 +2,5 @@
 
 struct secme_session
 {
-    int module;
-    int action;
-    char *args;
     struct secme_session *next;
-    char body[];
 };
--- src/secme.c
+++ src/secme.c
@@ -1,42 +1,50 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "list.h"
 #include "session.h"
 
-secme_list(struct secme_session) *sessions;
 atomic_bool done = false;
 
 void *producer(void *arg)
 {
-  struct secme_session session;
-  for(size_t i = 0; i < 10; ++i)
+  struct secme_session *_Atomic sessions = arg;
+  printf("d: %ld\n", (size_t)sessions);
+  
+  for(size_t i = 1; i < 11; ++i)
   {
-    secme_list_push(sessions, &session);
+    struct secme_session *_Atomic session = (struct secme_session *)malloc(sizeof(struct secme_session));
+    secme_list_push(sessions, session);
+    printf("d: %ld\n", (size_t)sessions);
   }
 
   atomic_store(&done, true);
-  (void) arg;
   pthread_exit((void *)0);
 }
 
 void *consumer(void *arg)
 {
-  while(!secme_list_empty(sessions)) {
-    secme_list_pop(sessions);
-    printf("!\n");
-  }
-
-  (void) arg;
+  struct secme_session *_Atomic sessions = arg;
+  // printf("c: %p\n", sessions);
+  
+  secme_list_pop(sessions);
+  atomic_store(&done, false);
+  (void)arg;
   pthread_exit((void *)0);
 }
 
 int main(void)
 {
-  pthread_t t1, t2;
-  pthread_create(&t1, NULL, producer, NULL);
-  pthread_create(&t2, NULL, consumer, NULL);
+  secme_list(struct secme_session) *_Atomic sessions = (__typeof(sessions))malloc(sizeof(*sessions));
+  
+  pthread_t t1;
+  pthread_t t2;
+  pthread_create(&t1, NULL, producer, sessions);
+  sleep(2);
+  pthread_create(&t2, NULL, consumer, sessions);
 
   pthread_join(t1, NULL);
   pthread_join(t2, NULL);
